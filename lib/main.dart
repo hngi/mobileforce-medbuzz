6e34d661cb468bddfcb11754e7266b9f09299435
revelationjay02@gmail.com <revelationjay02@gmail.com>
2020-06-20 12:58:19 +0100

    Added Signup screen, import to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:MedBuzz/ui/app_theme/app_theme.dart';
 import 'package:MedBuzz/ui/views/Home.dart';
 import 'package:MedBuzz/ui/views/profile_page.dart';
 import 'package:MedBuzz/ui/views/splash_screen.dart';
+import 'package:MedBuzz/ui/views/signup_screen.dart';
 
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
