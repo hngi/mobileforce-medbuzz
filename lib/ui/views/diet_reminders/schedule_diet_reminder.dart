c84e2e32e533d354d6654a4e5f02a0bfaa82490a
Abere Fejiro <aberefejiro@gmail.com>
2020-07-10 03:49:01 +0100

    Extra changes, more to come

--- lib/ui/views/diet_reminders/schedule_diet_reminder.dart
+++ lib/ui/views/diet_reminders/schedule_diet_reminder.dart
@@ -304,7 +304,7 @@ class AddDietReminderScreen extends StatelessWidget {
                             child: Text('Save',
                                 style: TextStyle(
                                     fontSize: Config.textSize(context, 5),
-                                    fontWeight: FontWeight.w500,
+                                    fontWeight: FontWeight.bold,
                                     color:
                                         Theme.of(context).primaryColorLight))),
                       )
