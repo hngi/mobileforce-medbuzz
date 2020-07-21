93289bcf19c88c4b620dbd51bf1e1b04d51ba391
iyiola akanbi <iyiola3332@gmail.com>
2020-07-21 04:22:40 +0100

    setting up fingerprint in activity

---
+++ lib/ui/views/auth/local_auth.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Auth extends StatefulWidget {
+  @override
+  _AuthState createState() => _AuthState();
+}
+
+class _AuthState extends State<Auth> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
