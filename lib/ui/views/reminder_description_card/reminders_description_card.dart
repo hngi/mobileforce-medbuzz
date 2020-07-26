572782b2061c473ab395c6992d82f59d281e50d1
Gideon Peters <gideonpeters85@gmail.com>
2020-07-26 11:25:38 +0100

    chore: made notifications in new reminders screen visible only if not skipped and if not done

--- lib/ui/views/reminder_description_card/reminders_description_card.dart
+++ lib/ui/views/reminder_description_card/reminders_description_card.dart
@@ -38,177 +38,185 @@ class _RemindersDescriptionCardState extends State<RemindersDescriptionCard> {
     // initializeDateFormatting();
     var userDB = Provider.of<UserCrud>(context);
     userDB.getuser();
-    return Padding(
-      padding: EdgeInsets.symmetric(vertical: Config.yMargin(context, 1.5)),
-      child: Container(
-          padding: EdgeInsets.symmetric(horizontal: Config.xMargin(context, 2)),
-          width: widget.width,
-          height: widget.height * .23,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(Config.xMargin(context, 6)),
-            boxShadow: [
-              BoxShadow(
-                color: Theme.of(context).primaryColorLight,
-                spreadRadius: 5,
-              ),
-            ],
-          ),
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                SizedBox(height: Config.yMargin(context, 1)),
-                Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    //circle avatar for reminder image
-                    CircleAvatar(
-                      child: Image.asset(
-                        model.getImage(widget.model, context),
-                        fit: BoxFit.cover,
-                      ),
-                      radius: Config.xMargin(context, 5.55),
-                      backgroundColor:
-                          Theme.of(context).primaryColor.withOpacity(.9),
-                    ),
-                    SizedBox(width: Config.xMargin(context, 4)),
-                    //vertical divider
-                    Container(
-                      color: Theme.of(context).primaryColorDark.withOpacity(.5),
-                      height: widget.height * 0.07,
-                      width: widget.width * 0.001,
-                      child: VerticalDivider(),
-                    ),
-                    SizedBox(width: Config.xMargin(context, 4)),
-                    //section for reminder name, time frame, date and time, and points earned
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Container(
-                          width: MediaQuery.of(context).size.width * .4,
-                          child: Text(
-                            model.getReminderName(widget.model),
-                            softWrap: true,
-                            overflow: TextOverflow.ellipsis,
-                            style: TextStyle(
-                                color: Theme.of(context).primaryColorDark,
-                                fontWeight: FontWeight.bold,
-                                fontSize: Config.textSize(context, 5)),
-                          ),
+    return Visibility(
+      //only shows models that have not been clicked by the user
+      visible: widget.model.isDone == false && !widget.model.isSkipped == false,
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: Config.yMargin(context, 1.5)),
+        child: Container(
+            padding:
+                EdgeInsets.symmetric(horizontal: Config.xMargin(context, 2)),
+            width: widget.width,
+            height: widget.height * .23,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(Config.xMargin(context, 6)),
+              boxShadow: [
+                BoxShadow(
+                  color: Theme.of(context).primaryColorLight,
+                  spreadRadius: 5,
+                ),
+              ],
+            ),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  SizedBox(height: Config.yMargin(context, 1)),
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      //circle avatar for reminder image
+                      CircleAvatar(
+                        child: Image.asset(
+                          model.getImage(widget.model, context),
+                          fit: BoxFit.cover,
                         ),
-                        SizedBox(height: Config.yMargin(context, 1)),
-                        Row(
-                          mainAxisSize: MainAxisSize.min,
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text(
-                              'Total points earned:',
+                        radius: Config.xMargin(context, 5.55),
+                        backgroundColor:
+                            Theme.of(context).primaryColor.withOpacity(.9),
+                      ),
+                      SizedBox(width: Config.xMargin(context, 4)),
+                      //vertical divider
+                      Container(
+                        color:
+                            Theme.of(context).primaryColorDark.withOpacity(.5),
+                        height: widget.height * 0.07,
+                        width: widget.width * 0.001,
+                        child: VerticalDivider(),
+                      ),
+                      SizedBox(width: Config.xMargin(context, 4)),
+                      //section for reminder name, time frame, date and time, and points earned
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Container(
+                            width: MediaQuery.of(context).size.width * .4,
+                            child: Text(
+                              model.getReminderName(widget.model),
+                              softWrap: true,
+                              overflow: TextOverflow.ellipsis,
                               style: TextStyle(
                                   color: Theme.of(context).primaryColorDark,
-                                  fontSize: Config.textSize(context, 3.85)),
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: Config.textSize(context, 5)),
                             ),
-                            SizedBox(width: Config.xMargin(context, 1.5)),
-                            Text(
-                              model.getPoints(widget.model),
+                          ),
+                          SizedBox(height: Config.yMargin(context, 1)),
+                          Row(
+                            mainAxisSize: MainAxisSize.min,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                'Total points earned:',
+                                style: TextStyle(
+                                    color: Theme.of(context).primaryColorDark,
+                                    fontSize: Config.textSize(context, 3.85)),
+                              ),
+                              SizedBox(width: Config.xMargin(context, 1.5)),
+                              Text(
+                                model.getPoints(widget.model),
+                                style: TextStyle(
+                                    color: Theme.of(context).primaryColor,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: Config.textSize(context, 3.85)),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: Config.yMargin(context, .9)),
+                          Row(
+                            children: <Widget>[
+                              //text widget for time
+                              Text(model.getTime(widget.model, context),
+                                  style: TextStyle(
+                                      fontSize: Config.textSize(context, 3))),
+                              SizedBox(width: Config.xMargin(context, 1)),
+                              //Text widget for date
+                              Text(model.getDate(widget.model),
+                                  style: TextStyle(
+                                      fontSize: Config.textSize(context, 3)))
+                            ],
+                          )
+                        ],
+                      ),
+                      SizedBox(width: Config.xMargin(context, 6.5)),
+                      //share button
+                      GestureDetector(
+                        onTap: () {
+                          Share.share(
+                              'I successfully gained ${model.getPoints(widget.model)} points on the Medbuzz App',
+                              subject: 'Medbuzz Progress Report');
+                        },
+                        child: Container(
+                            child: Column(children: <Widget>[
+                          Icon(
+                            Icons.share,
+                            color: Theme.of(context).primaryColor,
+                            size: Config.xMargin(context, 8.33),
+                          ),
+                          Text('Share',
                               style: TextStyle(
                                   color: Theme.of(context).primaryColor,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: Config.textSize(context, 3.85)),
-                            ),
-                          ],
-                        ),
-                        SizedBox(height: Config.yMargin(context, .9)),
-                        Row(
-                          children: <Widget>[
-                            //text widget for time
-                            Text(model.getTime(widget.model, context),
-                                style: TextStyle(
-                                    fontSize: Config.textSize(context, 3))),
-                            SizedBox(width: Config.xMargin(context, 1)),
-                            //Text widget for date
-                            Text(model.getDate(widget.model),
-                                style: TextStyle(
-                                    fontSize: Config.textSize(context, 3)))
-                          ],
-                        )
-                      ],
-                    ),
-                    SizedBox(width: Config.xMargin(context, 6.5)),
-                    //share button
-                    GestureDetector(
-                      onTap: () {
-                        Share.share(
-                            'I successfully gained ${model.getPoints(widget.model)} points on the Medbuzz App',
-                            subject: 'Medbuzz Progress Report');
-                      },
-                      child: Container(
-                          child: Column(children: <Widget>[
-                        Icon(
-                          Icons.share,
-                          color: Theme.of(context).primaryColor,
-                          size: Config.xMargin(context, 8.33),
-                        ),
-                        Text('Share',
-                            style: TextStyle(
-                                color: Theme.of(context).primaryColor,
-                                fontSize: Config.textSize(context, 3.85)))
-                      ])),
-                    ),
+                                  fontSize: Config.textSize(context, 3.85)))
+                        ])),
+                      ),
 
-                    //share section
-                  ],
-                ),
-                SizedBox(height: Config.yMargin(context, 1)),
-                Divider(
-                  color: Theme.of(context).primaryColorDark.withOpacity(.5),
-                  endIndent: 10,
-                  indent: 10,
-                ),
-                SizedBox(height: Config.yMargin(context, 1.5)),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: <Widget>[
-                    FlatButton.icon(
-                        //iconColor: Colors.green,
-                        label: Text("Skip"),
-                        icon: Icon(
-                          Icons.clear,
-                          color: Colors.red,
-                        ),
-                        onPressed: () {
-                          Flushbar(
-                            icon: Icon(
-                              Icons.info_outline,
-                              size: 28.0,
-                              color: Colors.black,
-                            ),
-                            //backgroundColor: Colors.red[400],
-                            message: "You don fuck up. Why you no train?",
-                            duration: Duration(seconds: 3),
-                          )..show(context);
-                        }),
-                    FlatButton.icon(
-                        //iconColor: Colors.green,
-                        label: Text("Done"),
-                        icon: Icon(Icons.check, color: Colors.green),
-                        onPressed: () {
-                          model.onDoneTap(widget.model, context);
-                          Flushbar(
-                            icon: Icon(
-                              Icons.info_outline,
-                              size: 28.0,
-                              color: Colors.black,
-                            ),
-                            backgroundColor: Colors.green,
-                            message: "We wish you a healthy life",
-                            duration: Duration(seconds: 3),
-                          )..show(context);
-                        }),
-                  ],
-                )
-              ])),
+                      //share section
+                    ],
+                  ),
+                  SizedBox(height: Config.yMargin(context, 1)),
+                  Divider(
+                    color: Theme.of(context).primaryColorDark.withOpacity(.5),
+                    endIndent: 10,
+                    indent: 10,
+                  ),
+                  SizedBox(height: Config.yMargin(context, 1.5)),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      FlatButton.icon(
+                          //iconColor: Colors.green,
+                          label: Text("Skip"),
+                          icon: Icon(
+                            Icons.clear,
+                            color: Colors.red,
+                          ),
+                          onPressed: () {
+                            model.onSkipTap(widget.model, context);
+                            Flushbar(
+                              icon: Icon(
+                                Icons.info_outline,
+                                size: 28.0,
+                                color: Colors.black,
+                              ),
+                              //backgroundColor: Colors.red[400],
+                              message:
+                                  "Hmmm, is this becoming a habit? You should complete your reminders more",
+                              duration: Duration(seconds: 3),
+                            )..show(context);
+                          }),
+                      FlatButton.icon(
+                          //iconColor: Colors.green,
+                          label: Text("Done"),
+                          icon: Icon(Icons.check, color: Colors.green),
+                          onPressed: () {
+                            model.onDoneTap(widget.model, context);
+                            Flushbar(
+                              icon: Icon(
+                                Icons.info_outline,
+                                size: 28.0,
+                                color: Colors.black,
+                              ),
+                              backgroundColor: Colors.green,
+                              message: "We wish you a healthy life",
+                              duration: Duration(seconds: 3),
+                            )..show(context);
+                          }),
+                    ],
+                  )
+                ])),
+      ),
     );
   }
 }
