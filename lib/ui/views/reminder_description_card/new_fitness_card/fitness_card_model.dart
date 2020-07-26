6e81216788c1d458b9e58f5a2372f546cbf02e35
Crazelu <eberelucky64@gmail.com>
2020-07-26 01:26:56 +0100

    feat: added function to increment points

--- lib/ui/views/reminder_description_card/new_fitness_card/fitness_card_model.dart
+++ lib/ui/views/reminder_description_card/new_fitness_card/fitness_card_model.dart
@@ -1,7 +1,15 @@
 import 'package:MedBuzz/core/models/fitness_reminder_model/fitness_reminder.dart';
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 
 class NewFitnessCardModel extends ChangeNotifier {
+  int _totalPoints;
+  int get totalPoints => _totalPoints;
+
+  int _nextBenchMark;
+  int get nextBenchMark => _nextBenchMark;
+  final String _boxName = 'fitnessPoints';
+  final String _key = 'points';
   String getReminderName(FitnessReminder reminderModel) {
     return reminderModel.fitnesstype;
   }
@@ -9,4 +17,23 @@ class NewFitnessCardModel extends ChangeNotifier {
   void getTime() {}
 
   void getDate() {}
+
+  void onDoneTap() async {
+    try {
+      var box = await Hive.openBox(_boxName);
+      int points = box.get(_key);
+      if (points == null) {
+        box.put(_key, 5);
+        _totalPoints = 5;
+        notifyListeners();
+      } else {
+        box.put(_key, points + 5);
+        _totalPoints = points + 5;
+        notifyListeners();
+      }
+      box.close();
+    } catch (e) {
+      print(e);
+    }
+  }
 }
